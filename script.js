af912105ab7be224109a5f42fdfca04d52746cd0
Jenny Dixon <45156191+jennyrmdixon@users.noreply.github.com>
2023-10-08 08:35:24 -0700

    Add recognition for winning player

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Module to create gameboard (array)
-
 const gameBoard = (() => {
-  let grid = ["", "", "", "", "", "O", "", "", ""];
+  let grid = ["", "", "", "", "", "", "", "", ""];
   let gridCells = document.getElementById('gameBoardWrapper').children;
   const updateGrid  = (() => {
   for (let i = 0; i < 9; i++) {
@@ -14,14 +13,14 @@ const gameBoard = (() => {
 }) ();
 
 //Factory function to create player
-
-
 const player = (name, marker) => {
-  const takeTurn = (id) => {
+  const takeTurn = (gridCell) => {
 
-    if (gameBoard.grid[id] === "") {
-      gameBoard.grid[id] = marker;
+    if (gameBoard.grid[gridCell] === "") {
+      gameBoard.grid[gridCell] = marker;
       gameBoard.updateGrid();
+      gameController.checkForWin(playerOne);
+      gameController.checkForWin(playerTwo);
       gameController.switchPlayer();
     }
   };
@@ -32,21 +31,53 @@ const player = (name, marker) => {
 let playerOne = player("Player One", "X");
 let playerTwo = player("Player Two", "O");
 
-// displayController
+// Module to contorl game flow
 const gameController  = (() => { 
   let currentPlayer = playerOne;
   const switchPlayer = () => {
   currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
 }
+
+const getAllIndexes = (arr, mark) => {
+  return arr.map((elm, idx) => elm === mark ? idx : '').filter(String);
+}
+
+const checkForWin = (player) => {
+  console.log("Win check" + player.marker);
+  let markerMap = getAllIndexes(gameBoard.grid, player.marker);
+  let checker = (arr, target) => target.every(v => arr.includes(v));
+
+  console.log(markerMap);
+
+  if (
+    checker(markerMap, [0,1,2]) ||
+    checker(markerMap, [3,4,5]) ||
+    checker(markerMap, [6,7,8]) ||
+    checker(markerMap, [0,3,6]) ||
+    checker(markerMap, [1,4,7]) ||
+    checker(markerMap, [2,5,8]) ||
+    checker(markerMap, [0,4,8]) ||
+    checker(markerMap, [2,4,6]) 
+  ) {
+    alert(player.name + " wins!");
+  }
+
+}
+
 const runGame = () => {
   gameBoard.updateGrid();
 
-  for (let i = 0; i < 9; i++) {
+   for (let i = 0; i < 9; i++) {
     gameBoard.gridCells[i].addEventListener("click", function () {
+      ///Move this to before click
+      // checkForWin(playerOne);
+      // checkForWin(playerTwo);
       currentPlayer.takeTurn(i);
-    });
-  }}
-   return {switchPlayer, runGame, currentPlayer};
+
+     });
+  }
+}
+   return {switchPlayer, runGame, checkForWin, currentPlayer};
 })();
 
 gameController.runGame();
