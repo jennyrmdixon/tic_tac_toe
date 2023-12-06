330f6d5cb38a84b26eb034f6858e861097d77642
Jenny Dixon <45156191+jennyrmdixon@users.noreply.github.com>
2023-12-05 20:16:27 -0800

    Minor code cleanup

--- script.js
+++ script.js
@@ -1,121 +1,111 @@
-//Can you reduce the number of vairalbe shared between functions?
-//Can use a method to return a variable instead of sharing?
-//Learn more about event listeners 
-
-
 //Module to handle layout of game grid
 const gameBoard = (() => {
-  let grid = ["", "", "", "", "", "", "", "", ""];
-  let gridCells = document.getElementById('gameBoardWrapper').children;
+  const GRID_SIZE = 9;
+  const grid = Array(GRID_SIZE).fill("");
+  const gridCells = document.getElementById("gameBoardWrapper").children;
 
   const updateGrid = () => {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < GRID_SIZE; i++) {
       let cellContent = gridCells.item(i);
-      cellContent.setAttribute('id', i);
+      cellContent.setAttribute("id", i);
       cellContent.textContent = grid[i];
     }
-  }
+  };
 
-  return {grid, gridCells, updateGrid}
+  return { grid, gridCells, updateGrid };
 })();
-
-
 
 //Factory function to create player
 const player = (name, marker) => {
   const takeTurn = (gridCell) => {
-
     if (gameBoard.grid[gridCell] === "") {
       gameBoard.grid[gridCell] = marker;
     }
   };
 
   return { name, marker, takeTurn };
-}
+};
 
-let playerOne = player("Player One", "X");
-let playerTwo = player("Player Two", "O");
-
-
-
+const playerOne = player("Player One", "X");
+const playerTwo = player("Player Two", "O");
 
 // Module to contorl game flow
-const gameController  = (() => { 
-  let statusHeading = document.getElementById('gameStatus');
+const gameController = (() => {
+  const statusHeading = document.getElementById("gameStatus");
   let currentPlayer = playerOne;
 
   const switchPlayer = () => {
-  currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
-  statusHeading.textContent = currentPlayer.name + "(" + currentPlayer.marker + ")'s"  + " Turn";
-}
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    statusHeading.textContent =
+      currentPlayer.name + "(" + currentPlayer.marker + ")'s" + " Turn";
+  };
 
-const getAllIndexes = (arr, mark) => {
-  return arr.map((elm, idx) => elm === mark ? idx : '').filter(String);
-}
+  const getAllIndexes = (arr, mark) => {
+    return arr.map((elm, idx) => (elm === mark ? idx : "")).filter(String);
+  };
 
-const checkForWin = (player) => {
-  let markerMap = getAllIndexes(gameBoard.grid, player.marker);
-  let checker = (arr, target) => target.every(v => arr.includes(v));
+  const checkForWin = (player) => {
+    const markerMap = getAllIndexes(gameBoard.grid, player.marker);
+    const checker = (arr, target) => target.every((v) => arr.includes(v));
 
-  /////Try another way to do this? Create a 2D array, and search for matching items
-  if (
-    checker(markerMap, [0,1,2]) ||
-    checker(markerMap, [3,4,5]) ||
-    checker(markerMap, [6,7,8]) ||
-    checker(markerMap, [0,3,6]) ||
-    checker(markerMap, [1,4,7]) ||
-    checker(markerMap, [2,5,8]) ||
-    checker(markerMap, [0,4,8]) ||
-    checker(markerMap, [2,4,6]) 
-  ) {
-    statusHeading.textContent = player.name + "(" + player.marker + ")"  + " Wins!";
-    statusHeading.classList.add('win');  
-    return true;                                    
-  }
+    /////Try another way to do this? Create a 2D array, and search for matching items
+    if (
+      checker(markerMap, [0, 1, 2]) ||
+      checker(markerMap, [3, 4, 5]) ||
+      checker(markerMap, [6, 7, 8]) ||
+      checker(markerMap, [0, 3, 6]) ||
+      checker(markerMap, [1, 4, 7]) ||
+      checker(markerMap, [2, 5, 8]) ||
+      checker(markerMap, [0, 4, 8]) ||
+      checker(markerMap, [2, 4, 6])
+    ) {
+      statusHeading.textContent =
+        player.name + "(" + player.marker + ")" + " Wins!";
+      statusHeading.classList.add("win");
+      return true;
+    }
+  };
 
-}
-
-const runGame = () => {
-
-   gameBoard.updateGrid();
-    for (let i = 0; i < 9; i++) {
-      gameBoard.gridCells[i].addEventListener("click", runTurn);
-    };
-
-   function runTurn() {
-    currentPlayer.takeTurn(this.id);//add code to take turn in here?
+  const runGame = () => {
     gameBoard.updateGrid();
-    switchPlayer();
 
+    function runTurn() {
+      currentPlayer.takeTurn(this.id);
+      gameBoard.updateGrid();
+      switchPlayer();
 
-    if (checkForWin(playerOne) || checkForWin(playerTwo)) {
-      for (let i = 0; i < 9; i++) {
-        gameBoard.gridCells[i].removeEventListener("click", runTurn);
+      if (checkForWin(playerOne) || checkForWin(playerTwo)) {
+        for (let i = 0; i < 9; i++) {
+          gameBoard.gridCells[i].removeEventListener("click", runTurn);
+        }
       }
     }
-   }
 
-  resetButton = document.getElementById("reset");
-  resetButton.addEventListener("click", () => {    
-    for (let i = 0; i < 9; i++) {
-      let cellContent = gameBoard.gridCells.item(i);
-      cellContent.textContent = "";
-      gameBoard.grid[i] = "";
-      gameBoard.gridCells[i].removeEventListener("click", runTurn);
-      statusHeading.classList.remove('win');  
-      statusHeading.textContent =  currentPlayer.name + "(" + currentPlayer.marker + "), Click to Start!"
-    }
     for (let i = 0; i < 9; i++) {
       gameBoard.gridCells[i].addEventListener("click", runTurn);
-    };
-  
-  })
-}
+    }
 
+    resetButton = document.getElementById("reset");
+    resetButton.addEventListener("click", () => {
+      for (let i = 0; i < 9; i++) {
+        let cellContent = gameBoard.gridCells.item(i);
+        cellContent.textContent = "";
+        gameBoard.grid[i] = "";
+        gameBoard.gridCells[i].removeEventListener("click", runTurn);
+        statusHeading.classList.remove("win");
+        statusHeading.textContent =
+          currentPlayer.name +
+          "(" +
+          currentPlayer.marker +
+          "), Click to Start!";
+      }
+      for (let i = 0; i < 9; i++) {
+        gameBoard.gridCells[i].addEventListener("click", runTurn);
+      }
+    });
+  };
 
-return {runGame};
+  return { runGame };
 })();
 
 gameController.runGame();
-
-
